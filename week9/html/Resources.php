111c0a71bfe22b479043a52875470cf64cee01f9
Jamzy Marie Cuyugan <jtcuyugan@student.apc.edu.ph>
2023-01-31 13:40:57 +0800

    Update Resources.php

--- week9/html/Resources.php
+++ week9/html/Resources.php
@@ -81,7 +81,7 @@
 
 			<h2>PHP Form Validation Example</h2>
 			<p><span class="error">* required field</span></p>
-			<form method="post" action="<?php echo phpspecialchars($_SERVER["PHP_SELF"]);?>">  
+			<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
 			Name: <input type="text" name="name" value="<?php echo $name;?>">
 			<span class="error">* <?php echo $nameErr;?></span>
 			<br><br>
