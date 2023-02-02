f01ec93c422281874a8dd93b0c9ccce1d445265a
Jamzy <119426865+jtcuyugan@users.noreply.github.com>
2023-02-02 13:11:33 +0800

    Update Resources.php

--- week9/html/Resources.php
+++ week9/html/Resources.php
@@ -32,20 +32,20 @@
 
 			if ($_SERVER["REQUEST_METHOD"] == "POST") {
 			if (empty($_POST["name"])) {
-				$nameErr = "Name is required";
+				$nameErr = "The Star must be named.";
 			} else {
 				$name = test_input($_POST["name"]);
 				if (!preg_match("/^[a-zA-Z-' ]*$/",$name)) {
-				$nameErr = "Only letters and white space allowed";
+				$nameErr = "Only letters and white space allowed in this part of the galaxy.";
 				}
 			}
 			
 			if (empty($_POST["email"])) {
-				$emailErr = "Email is required";
+				$emailErr = "Coordinates are";
 			} else {
 				$email = test_input($_POST["email"]);
 				if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-				$emailErr = "Invalid email format";
+				$emailErr = "Invalid coordinate format";
 				}
 			}
 				
@@ -54,7 +54,7 @@
 			} else {
 				$website = test_input($_POST["website"]);
 				if (!preg_match("/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i",$website)) {
-				$websiteErr = "Invalid URL";
+				$websiteErr = "Invalid Station";
 				}
 			}
 
@@ -65,7 +65,7 @@
 			}
 
 			if (empty($_POST["gender"])) {
-				$genderErr = "Gender is required";
+				$genderErr = "Identity is required";
 			} else {
 				$gender = test_input($_POST["gender"]);
 			}
@@ -79,21 +79,21 @@
 			}
 			?>
 
-			<h2>PHP Form Validation Example</h2>
-			<p><span class="error">* required field</span></p>
+			<h2>Register a Star!</h2>
+			<p style="font-size: 55px; font-family: joane_stencilregular; color: white;" id="lobbytext"><span class="error">* This field is required</span></p>
 			<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
-			Name: <input type="text" name="name" value="<?php echo $name;?>">
+			Star's Name: <input type="text" name="name" value="<?php echo $name;?>">
 			<span class="error">* <?php echo $nameErr;?></span>
 			<br><br>
-			E-mail: <input type="text" name="email" value="<?php echo $email;?>">
+			Star's Coordinates: <input type="text" name="email" value="<?php echo $email;?>">
 			<span class="error">* <?php echo $emailErr;?></span>
 			<br><br>
-			Website: <input type="text" name="website" value="<?php echo $website;?>">
+			Star's Station: <input type="text" name="website" value="<?php echo $website;?>">
 			<span class="error"><?php echo $websiteErr;?></span>
 			<br><br>
 			Comment: <textarea name="comment" rows="5" cols="40"><?php echo $comment;?></textarea>
 			<br><br>
-			Gender:
+			Star's Identity:
 			<input type="radio" name="gender" <?php if (isset($gender) && $gender=="female") echo "checked";?> value="female">Female
 			<input type="radio" name="gender" <?php if (isset($gender) && $gender=="male") echo "checked";?> value="male">Male
 			<input type="radio" name="gender" <?php if (isset($gender) && $gender=="other") echo "checked";?> value="other">Other  
