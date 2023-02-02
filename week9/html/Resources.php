61bcc92e835493b05506bc59ba5a578511831e0c
Jamzy <119426865+jtcuyugan@users.noreply.github.com>
2023-02-02 13:19:33 +0800

    Update Resources.php

--- week9/html/Resources.php
+++ week9/html/Resources.php
@@ -80,9 +80,9 @@
 			?>
 
 			<h2 style="font-family: joane_stencilregular; color:white;">Register a Star!</h2>
-			<p style="font-size: 55px; font-family: TimesNewRoman; color: white;" id="lobbytext"><span class="error">* This field is required</span></p>
+			<p style="font-size: 20px; font-family: TimesNewRoman; color: white;" id="lobbytext"><span class="error">* This field is required</span></p>
 			<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
-			<p style="font-size: 30px; font-family: TimesNewRoman; color: white;">Star's Name:</p><input type="text" name="name" value="<?php echo $name;?>">
+			<p style="font-size: 20px; font-family: TimesNewRoman; color: white;">Star's Name:</p><input type="text" name="name" value="<?php echo $name;?>">
 			<span class="error">* <?php echo $nameErr;?></span>
 			<br><br>
 			Star's Coordinates: <input type="text" name="email" value="<?php echo $email;?>">
