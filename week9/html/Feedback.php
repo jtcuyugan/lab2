c0f176ad3e2de211969b033adc861aa6cc638320
Jamzy Marie Cuyugan <jtcuyugan@student.apc.edu.ph>
2023-02-03 23:09:24 +0800

    Update Feedback.php

--- week9/html/Feedback.php
+++ week9/html/Feedback.php
@@ -99,8 +99,8 @@
             <p style="font-size: 25px; font-family: TimesNewRoman; color: white;" id="restext"><textarea name="comment" rows="5" cols="40"><?php echo $comment;?></textarea>
             <span class="error">* <?php echo $commentErr;?></span></p>
 			<br>
-			<p style="font-size: 20px; font-family: TimesNewRoman; color: white;" id="restext">Star's Identity:
-            <p style="font-size: 20px; font-family: TimesNewRoman; color: white;" id="restext">
+			<p style="font-size: 25px; font-family: TimesNewRoman; color: white;" id="restext">Star's Identity:
+            <p style="font-size: 25px; font-family: TimesNewRoman; color: white;" id="restext">
 			<input type="radio" name="gender" <?php if (isset($gender) && $gender=="female") echo "checked";?> value="female">Female
 			<input type="radio" name="gender" <?php if (isset($gender) && $gender=="male") echo "checked";?> value="male">Male
 			<input type="radio" name="gender" <?php if (isset($gender) && $gender=="other") echo "checked";?> value="other">Other  
