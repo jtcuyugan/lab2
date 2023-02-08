9ea42645474c92947ffae6f8b6b7040e34996551
Jamzy Marie Cuyugan <jtcuyugan@student.apc.edu.ph>
2023-02-09 00:23:24 +0800

    Update SQLFeed.php

--- week9/html/SQLFeed.php
+++ week9/html/SQLFeed.php
@@ -5,7 +5,7 @@
 			// Code from: https://www.geeksforgeeks.org/how-to-insert-form-data-into-database-using-php/
 			// database connection code
 			$con = mysqli_connect('localhost', 'webprogmi212', 'webprogmi212','webprogmi212');
-			
+
 			if($con === false){
 				die("ERROR: Could not connect. "
 					. mysqli_connect_error());
@@ -19,7 +19,7 @@
 			$gender = $_REQUEST['gender'];
 
 			// database insert SQL code
-			$sql = "INSERT INTO jtcuyugan_stars VALUES ('$name', '$email', '$website', '$comment', '$gender')";
+			$sql = "INSERT INTO jtcuyugan_stars VALUES (NULL, '$name', '$email', '$website', '$comment', '$gender' , current_timestamp()))";
 			
 			if(mysqli_query($con, $sql)){
 				echo "<script> location.href='Feedback.php'; </script>";
