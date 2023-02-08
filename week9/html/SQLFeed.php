7e2615fd627760b0aa48280e26b49a66ea18dcef
Jamzy Marie Cuyugan <jtcuyugan@student.apc.edu.ph>
2023-02-09 00:22:29 +0800

    Updating

--- week9/html/SQLFeed.php
+++ week9/html/SQLFeed.php
@@ -5,6 +5,7 @@
 			// Code from: https://www.geeksforgeeks.org/how-to-insert-form-data-into-database-using-php/
 			// database connection code
 			$con = mysqli_connect('localhost', 'webprogmi212', 'webprogmi212','webprogmi212');
+			
 			if($con === false){
 				die("ERROR: Could not connect. "
 					. mysqli_connect_error());
